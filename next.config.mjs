d6d83b27a4f11be99194c2131b14d217ea5992e8
Eloi Dieme <eloidieme@gmail.com>
2024-02-22 00:57:32 +0100

    workflows for pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    basePath: "/portfolio-website",
+    output: "export",  // <=== enables static exports
+  };
 
 export default nextConfig;
