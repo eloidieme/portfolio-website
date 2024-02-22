432a872e426cf31534d512a80f844a25b815aa5f
Eloi Dieme <eloidieme@gmail.com>
2024-02-22 01:35:43 +0100

    fix images on deployed

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,11 @@
 const nextConfig = {
     basePath: "/portfolio-website",
     output: "export",  // <=== enables static exports
+    images: {
+        loader: 'akamai',
+        path: '',
+      },
+    assetPrefix: './',
   };
 
 export default nextConfig;
