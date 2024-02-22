ab8e54c691f89b2fda67c416ef6371aa3d7891d9
Eloi Dieme <eloidieme@gmail.com>
2024-02-22 01:51:22 +0100

    fix config

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@
 const isProd = process.env.NODE_ENV = 'production'
 
 const nextConfig = {
-    basePath: isProd ? "/portfolio-website/" : "",
+    basePath: isProd ? "/portfolio-website" : "",
     assetPrefix: isProd ? '/portfolio-website/' : '',
     output: "export",  // <=== enables static exports
     images: {
