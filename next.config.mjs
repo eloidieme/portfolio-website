92e332e1478e3e80b598940fdec67954bd73b32a
Eloi Dieme <eloidieme@gmail.com>
2024-02-22 02:07:58 +0100

    revert config

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 
-const isProd = process.env.NODE_ENV = 'production'
 
 const nextConfig = {
-    basePath: isProd ? "/portfolio-website" : "",
-    assetPrefix: isProd ? '/portfolio-website/' : '',
     output: "export",  // <=== enables static exports
     images: {
         unoptimized: true
